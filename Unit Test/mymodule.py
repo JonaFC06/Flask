e9e25fe816eb056b98c0ec0ab078b04c62167833
Jonathan D. Fuentes Calvo <fuentesjonathan2003@gmail.com>
2026-01-10 19:00:25 -0600

    Add files via upload

---
+++ Unit Test/mymodule.py
@@ -0,0 +1,7 @@
+def square(number):
+    # Returns the square of a given number
+    return number ** 2
+
+def double(number):
+    # Returns twice the value of a given number
+    return number * 2
